40fbe32a6c7478dcdb873452cc4fef2ab78fe4de
gabrielegiuli <gabriele.giuli18@imperial.ac.uk>
2023-02-16 16:07:31 +0100

    improved ui

--- src/App.jsx
+++ src/App.jsx
@@ -91,15 +91,15 @@ function BarGraph({ data, authEmails }) {
   })
 
   return (
-    <ComposedChart width={850} height={400} data={cumulative}>
+    <ComposedChart width={850} height={400} data={cumulative} className='recharts-wrapper'>
       <XAxis dataKey="stamp" />
       <YAxis label={{ value: 'New Commits', angle: -90, position: 'insideLeft' }} yAxisId="left" />
       <YAxis label={{ value: 'New Commits', angle: -90, position: 'insideLeft' }} yAxisId="right" orientation="right" />
-      <Bar type="monotone" dataKey="Authors" barSize={30} fill="#8884d8" yAxisId="left" stackId="a"/>
-      <Bar type="monotone" dataKey="NonAuthors" barSize={30} fill="#82ca9d" yAxisId="left" stackId="a"/>
+      <Bar type="monotone" dataKey="Authors" barSize={30} fill="#82ca9d" yAxisId="left" stackId="a" />
+      <Bar type="monotone" dataKey="NonAuthors" barSize={30} fill="#8884d8" yAxisId="left" stackId="a" />
       <Line type="monotone" dataKey="Total" yAxisId="right" />
-      <Legend/>
-      <Tooltip/>
+      <Legend />
+      <Tooltip />
     </ComposedChart>
   )
 }
@@ -219,6 +219,7 @@ export default function App() {
   }
 
   const updateData = () => {
+    setAuthEmails([])
     getCommits(name, DEFAULT_MAX_REQUEST)
       .then(result => {
         updateCommitters(result)
@@ -229,15 +230,17 @@ export default function App() {
 
   return (
     <div>
-      <div className='search'>
-        <div className='component'>
-          <input type="text" placeholder="Repo Name..." className="input w-full max-w-xs" value={name} onChange={evt => setName(evt.target.value)} />
-        </div>
-        <div className='component'>
-          <button className="btn btn-primary" onClick={updateData}>Button</button>
+      <div className="form-control">
+        <div className="input-group">
+          <input type="text" placeholder="Search…" className="input input-bordered" value={name} onChange={evt => setName(evt.target.value)}/>
+          <button className="btn btn-square"  onClick={updateData}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+          </button>
         </div>
       </div>
-      <BarGraph data={commits} authEmails={authEmails} />
+      <div className='recharts-container'>
+        <BarGraph data={commits} authEmails={authEmails} />
+      </div>
       <EmailTable list={list} handle={updateSelected} />
     </div>
   )
