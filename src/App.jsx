57479edf78d3b16ab2a628da2a6e737c94ef8d5a
gabrielegiuli <gabriele.giuli18@imperial.ac.uk>
2023-02-22 18:29:47 +0100

    fixed bar glitch

--- src/App.jsx
+++ src/App.jsx
@@ -123,7 +123,7 @@ export default function App() {
           <div className="form-control">
             <div className="input-group">
               <input type="text" placeholder="Search…" className="input input-bordered w-full" value={name} onChange={evt => setName(evt.target.value)} />
-              <input type="text" placeholder="Access Token (Optional)" className="input input-bordered w-full max-w-xs" value={token != null ? token : ""} onChange={evt => setToken(evt.target.value)} />
+              <input type="text" placeholder="Access Token (Optional)" className="input input-bordered w-full" value={token != null ? token : ""} onChange={evt => setToken(evt.target.value)} />
               <button className="btn btn-square" onClick={updateData}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
               </button>
